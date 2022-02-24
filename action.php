4e66c789e6194bbdc3a4c8fedfac0cafe1cbacc1
ToniPortal <tonitoctoc13@gmail.com>
2022-02-24 11:51:16 +0100

    rs

--- action.php
+++ action.php
@@ -4,15 +4,16 @@ COLLUMN : <?php echo $_POST['col'] ?>
 
 <?php
 try {
-        $connection = new PDO('mysql:host=localhost;dbname=test', 'rien', '');
+        $connection = new PDO('mysql:host=localhost;dbname=test', 'root', '');
 
         $acteur = htmlspecialchars($_POST['from']);
 
         $requetelistepays = "SELECT * FROM `" . $acteur . '`';
 
         echo $requetelistepays;
+        $rs = $connection->query($requetelistepays);
 
-        foreach ($connection->query($requetelistepays) as $lesPays) {
+        foreach ($rs as $lesPays) {
                 print '<h4>' . $lesPays[htmlspecialchars($_POST['col'])] . "\n</h4>";
         }
 } catch (PDOException $e) {
